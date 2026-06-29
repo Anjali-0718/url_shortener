baf0aa5764b87434f90fa6daabc237bb1b62da9f
Anjali <ajanjali07@gmail.com>
2026-06-29 21:04:05 +0530

    Update encoder.cpp

--- src/core/encoder.cpp
+++ src/core/encoder.cpp
@@ -1,7 +1,6 @@
 #include "encoder.hpp"
 #include <algorithm>
 #include <stdexcept>
-
 using namespace std;
 
 string Base62Encoder::encode(long long id) {
@@ -9,21 +8,22 @@ string Base62Encoder::encode(long long id) {
     string result;
     result.reserve(7);
     while (id > 0) {
-        result += ALPHABET[id%62];
-        id /= 62;
+        result+=ALPHABET[id%62];
+        id/=62;
     }
     reverse(result.begin(), result.end());
     return result;
 }
 
 long long Base62Encoder::decode(string_view short_link) {
-    long long id = 0;
-    for (char c : short_link) {
-        size_t pos = ALPHABET.find(c);
-        if (pos == string_view::npos) {
-            throw invalid_argument("Invalid character");
+    long long id=0;
+    
+    for (char c:short_link) {
+        size_t pos=ALPHABET.find(c);
+        if (pos==string_view::npos) {
+            throw invalid_argument("Invalid");
         }
-        id = id * 62 + pos;
+        id=id*62+pos;
     }
     return id;
 }
